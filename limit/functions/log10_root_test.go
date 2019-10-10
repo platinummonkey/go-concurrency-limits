140e296c7cfa0a3d7873720cb2b763ed6adc5ac9
Wreulicke <masaya2dies@gmail.com>
2019-10-10 13:36:47 +0900

    fix tests

--- limit/functions/log10_root_test.go
+++ limit/functions/log10_root_test.go
@@ -12,19 +12,19 @@ func TestLog10RootFunction(t *testing.T) {
 	t.Run("ZeroIndex", func(t2 *testing.T) {
 		t2.Parallel()
 		f := Log10RootFunction(4)
-		assert.Equal(t2, 4, f(0))
+		assert.Equal(t2, 5, f(0))
 	})
 
 	t.Run("MaxIndex", func(t2 *testing.T) {
 		t2.Parallel()
 		f := Log10RootFunction(4)
-		assert.Equal(t2, 31, f(1000))
+		assert.Equal(t2, 7, f(1000))
 	})
 
 	t.Run("OutOfLookupRange", func(t2 *testing.T) {
 		t2.Parallel()
 		f := Log10RootFunction(4)
-		assert.Equal(t2, 50, f(2500))
+		assert.Equal(t2, 7, f(2500))
 	})
 }
 
@@ -34,18 +34,18 @@ func TestLog10RootFloatFunction(t *testing.T) {
 	t.Run("ZeroIndex", func(t2 *testing.T) {
 		t2.Parallel()
 		f := Log10RootFloatFunction(4)
-		assert.Equal(t2, 4.0, f(0))
+		assert.Equal(t2, 5.0, f(0))
 	})
 
 	t.Run("MaxIndex", func(t2 *testing.T) {
 		t2.Parallel()
 		f := Log10RootFloatFunction(4)
-		assert.InDelta(t2, 31.623, f(1000), 0.001)
+		assert.InDelta(t2, 7.0, f(1000), 0.001)
 	})
 
 	t.Run("OutOfLookupRange", func(t2 *testing.T) {
 		t2.Parallel()
 		f := Log10RootFloatFunction(4)
-		assert.Equal(t2, 50.0, f(2500))
+		assert.Equal(t2, 7.3979400086720375, f(2500))
 	})
 }
