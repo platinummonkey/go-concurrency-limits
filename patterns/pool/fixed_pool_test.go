79288c8801def67ac8e47e91fb4f8c7cbcec519f
Cody Lee <cody.lee@datadoghq.com>
2019-12-29 14:34:47 -0600

    add testing to pools
    some additional formatting
    
    fix  bug for timeout=0 on blocking limiter

---
+++ patterns/pool/fixed_pool_test.go
@@ -0,0 +1,45 @@
+package pool
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFixedPool(t *testing.T) {
+	asrt := assert.New(t)
+	p, err := NewFixedPool(
+		"test-fixed-pool",
+		10,
+		-1,
+		-1,
+		-1,
+		-1,
+		0,
+		nil,
+		nil,
+	)
+	asrt.NoError(err)
+
+	asrt.Equal(10, p.Limit())
+
+	var wg sync.WaitGroup
+	for i := 0; i < 20; i++ {
+		wg.Add(1)
+		go func(c int) {
+			defer wg.Done()
+			l, _ := p.Acquire(context.WithValue(context.Background(), "id", fmt.Sprint(c)))
+			log.Printf("acquired now, sleeping - %d\n", c)
+			time.Sleep(time.Millisecond * 100)
+			l.OnSuccess()
+			log.Printf("no longer acquired, released - %d\n", c)
+		}(i)
+		time.Sleep(time.Millisecond * 5)
+	}
+	wg.Wait()
+}
---
+++ patterns/pool/lifo_fixed_pool_test.go
@@ -0,0 +1,46 @@
+package pool
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLIFOFixedPool(t *testing.T) {
+	asrt := assert.New(t)
+	p, err := NewLIFOFixedPool(
+		"test-lifo-fixed-pool",
+		10,
+		-1,
+		-1,
+		-1,
+		-1,
+		0,
+		0,
+		nil,
+		nil,
+	)
+	asrt.NoError(err)
+
+	asrt.Equal(10, p.Limit())
+
+	var wg sync.WaitGroup
+	for i := 0; i < 20; i++ {
+		wg.Add(1)
+		go func(c int) {
+			defer wg.Done()
+			l, _ := p.Acquire(context.WithValue(context.Background(), "id", fmt.Sprint(c)))
+			log.Printf("acquired now, sleeping - %d\n", c)
+			time.Sleep(time.Millisecond * 100)
+			l.OnSuccess()
+			log.Printf("no longer acquired, released - %d\n", c)
+		}(i)
+		time.Sleep(time.Millisecond * 5)
+	}
+	wg.Wait()
+}
